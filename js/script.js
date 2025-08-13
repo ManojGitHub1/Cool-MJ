b346a36f53a54c9f257a7eb8c7d7378df65aac6a
ManojGitHub1 <manojjivanagi@gmail.com>
2025-08-13 14:41:18 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // Initialize Animate On Scroll (AOS) Library
     AOS.init({
-        duration: 800,          // Animation duration in milliseconds
-        easing: 'ease-in-out',  // Type of easing
-        once: true,             // Whether animation should happen only once
-        mirror: false           // Whether elements should animate out while scrolling past
+        duration: 800,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
     });
 
 });
